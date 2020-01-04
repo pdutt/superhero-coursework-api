4531fea9853e1dcd73e66762eb9a38a3e6bcc379
unknown <pdutt@wisc.edu>
2020-01-04 13:25:23 -0600

    Created project

---
+++ src/main/java/SuperheroCoursework/com/SuperheroCourseworkApplication.java
@@ -0,0 +1,13 @@
+package SuperheroCoursework.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroCourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroCourseworkApplication.class, args);
+	}
+
+}
